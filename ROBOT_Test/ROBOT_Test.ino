de9774fa90a6f1fb0faf197f0641b2ecb2fd747f
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2021-09-27 19:07:56 -0500

    Update ROBOT_Test.ino

--- ROBOT_Test/ROBOT_Test.ino
+++ ROBOT_Test/ROBOT_Test.ino
@@ -3,20 +3,21 @@
 
 KNWRobot *robot;
 bool startBump;
-int IRSensorPin = 15;
+int IRSensorPin = 15; //choose your IR Pin
+
 void setup()
 {
-    robot = new KNWRobot(0x3F);
-    Serial.begin(9600);
+    robot = new KNWRobot(0x3F); //Creates a new robot
+    Serial.begin(9600); // For printing
 }
 
 void loop()
 {
-    int numCharsReadFromIR = scanIR(IRSensorPin);
+    int numCharsReadFromIR = scanIR(IRSensorPin); //read the input from IR Receiver
     if (numCharsReadFromIR != 0){
     // Now print the reading on the next line of the LCD
     //Serial.print(numCharsReadFromIR);
     char* IRCharacters = getIR();
-    Serial.print(IRCharacters);
+    Serial.print(IRCharacters); //Get the character from the nums read from IR Receiver
     }
 }
